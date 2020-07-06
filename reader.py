d4b60e84e652bb5e7e1eefbedba1ab77a5d0a14e
Nafisa Tabassum <nafisatabassum@3d-p.com>
2020-07-05 18:37:29 -0600

    rough implementation of DFS search for extracting nodes that most likely contain an ingredient/instruction

--- reader.py
+++ reader.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 import requests
 import json
 import argparse
-# import re
+import nltk
+import re
+from pprint import pprint
 
 
 def get_parsed_html():
@@ -15,6 +18,11 @@ def get_parsed_html():
 
 
 # TODO: Convert html entities to ascii using BeautifulSoup
+def html_entity_converter(text):
+    text = BeautifulSoup(text, features="html.parser")
+    return text
+
+
 def jsonldschema(search_dict, field):
     found_field = []
     for k, v in search_dict.items():
@@ -33,7 +41,51 @@ def jsonldschema(search_dict, field):
             more_results = jsonldschema(v, field)
             for result in more_results:
                 found_field.append(result)
+
     return found_field
+
+
+def parse_microdata(field):
+    '''Search for tags with itemprop=""'''
+    pass
+
+
+def parse_RDFa(field):
+    '''Search for tags with property=""'''
+    pass
+
+
+def dfs(tree, ingreds, instr):
+    units = ["cup", "lb", "oz", "tsp", "tbsp", "ml"]
+    food_items = ["salt", "pepper", "butter", "oil", "eggs"]
+    for node in tree.children:
+        if isinstance(node, NavigableString):
+            s = str(node).strip()
+            if len(s) < 100:
+                if any(x in s for x in food_items) or any(x in s for x in units):
+                    ingreds[node.parent] = ""
+
+            elif len(s) < 400:
+                words = ["mix", "heat", "sprinkle",
+                         "bake", "cut", "serve", "whisk"]
+                if s[0].isdigit():
+                    new_s = re.sub(r"^[\d-]*", "", s).lstrip()
+                    if any(x in new_s for x in words):
+                        instr[node.parent] = ""
+                elif any(x in s for x in words):
+                    instr[node.parent] = ""
+            continue
+         # Compute LCA node and get class attr for tag
+        dfs(node, ingreds, instr)
+
+
+def lca():
+    pass
+
+
+def parse_arbitrary_html():
+    '''html that doesn't follow recipe schema'''
+    pass
 
 
 class Recipe:
@@ -48,22 +100,59 @@ class Recipe:
         else:
             return self.parsed_html.title.text
 
-    def cooktime(self):
+    def description(self):
         pass
+
+    def cooktime(self):
+        data = get_parsed_html().find_all(type="application/ld+json")
+        for item in data:
+            extracted_ingredients = jsonldschema(
+                json.loads(item.string), "recipeIngredient")
+            if not extracted_ingredients:
+                pass
 
     def servings(self):
         pass
 
     def ingredients(self):
-        print(self.parsed_html.findChild())
+        data = get_parsed_html().find_all(type="application/ld+json")
+        for item in data:
+            extracted_ingredients = jsonldschema(
+                json.loads(item.string), "recipeIngredient")
+            if not extracted_ingredients:
+                pass
+        # Further processing
+        processed = []
+        for ingredient in extracted_ingredients[0]:
+            ingredient = html_entity_converter(ingredient)
+            processed.append(ingredient)
+        return processed
+
+    def instructions(self):  # conditionals for nested dicts
+        data = get_parsed_html().find_all(type="application/ld+json")
+        for item in data:
+            extracted_instructions = jsonldschema(
+                json.loads(item.string), "recipeInstructions")
+            if not extracted_instructions:
+                pass
+        # Further processing
+        processed = []
+        for instruction in extracted_instructions[0]:
+            if isinstance(instruction, dict):
+                for k, v in instruction.items():
+                    if k == "text":
+                        processed.append(v)
+            else:
+                processed.append(instruction)
+        return processed
 
 
 def main():
-    data = json.loads(get_parsed_html().find(
-        "script", type="application/ld+json").string)
-    # with open("recipe.json", "w") as json_file:
-    #     json.dump(data, json_file, indent=4, separators=(",", ": "))
-    print(jsonldschema(data, "recipeInstructions"))
+    ingreds = {}
+    instr = {}
+    dfs(get_parsed_html().body, ingreds, instr)
+    print(ingreds)
+    print(instr)
 
 
 if __name__ == "__main__":
